a6941ad7842ca0bb06da3208f1b8d85ec0f714f0
Andrew Miloslavsky <amiloslavsky@gmail.com>
2017-09-19 11:09:22 -0400

    create wrapper for requests

--- geoquery_queries.py
+++ geoquery_queries.py
@@ -1,195 +1,211 @@
 #!/usr/bin/python2.7
 
-"""geoquery_queries.py: Queries for the GeoQuery Requests Web Application."""
+"""geoquery_queries.py: Wrapper class for accessing queries for the GeoQuery Requests."""
 
-def get_field(field, document):
-    """Get a field from a document.
+class GeoQueryRequests:
     
-    Args:
-        field: the field.
-        document: the document.
-    Returns:
-        value of the field in the document.
-    """
-    return document[field]
+    def __init__(self, requests):
+        self.requests = requests
 
-def compress(dataset):
-    """Compress values to 1 of each.
-    
-    Args:
-        dataset: set of data.
-    Returns:
-        set with 1 of each value.
-    """
-    return set(dataset)
+    def get_field(self, field, document):
+        """Get a field from a document.
+        
+        Args:
+            field: the field.
+            document: the document.
+        Returns:
+            value of the field in the document.
+        """
+        return document[field]
 
-def get_frequency(field, value, requests):
-    """Get frequency of a field value from requests.
-    
-    Args:
-        field: field to identify.
-        value: sought after value of field.
-        requests: list of requests.
-    Returns:
-        count of the occurence of the value.
-    """
-    count = 0
-    for request in requests:
-        if request[field] == value:
-            count += 1
-    
-    return count
+    def compress(self, dataset):
+        """Compress values to 1 of each.
+        
+        Args:
+            dataset: set of data.
+        Returns:
+            set with 1 of each value.
+        """
+        return set(dataset)
 
-def get_emails(requests):
-    """Get emails from requests. 
-    
-    Args:
-        requests: all requests from database.
-    Returns:
-        list of emails - not guaranteed to be a unique list.
-    """
-    emails = []
-    
-    for request in requests.find():
-        emails.append(request['email'])
+    def get_frequency(self, field, value):
+        """Get frequency of a field value from requests.
+        
+        Args:
+            field: field to identify.
+            value: sought after value of field.
+            requests: list of requests.
+        Returns:
+            count of the occurence of the value.
+        """
+        # count = 0
+        # for request in self.requests:
+        #     if request[field] == value:
+        #         count += 1
 
-    return emails
+        return self.requests.find({field: value}).count()
 
-def unique_users(requests):
-    """Get emails from requests. 
-    
-    Args:
-        requests: all requests from database.
-    Returns:
-        list of unique users.
-    """
-    uniq = set()
-    for request in requests.find():
-        email = str(request['email']).lower()
-        uniq.add(email)
-    
-    return uniq
+    def get_emails(self):
+        """Get emails from requests.
+        
+        Args:
+            requests: all requests from database.
+        Returns:
+            list of emails - not guaranteed to be a unique list.
+        """
+        emails = []
+        
+        for request in self.requests.find():
+            emails.append(request['email'])
 
-def total_requests(requests):
-    """Get total number of requests from the requests.
-    
-    Args:
-        requests: all requests from database.
-    Returns:
-        number of requests
-    """
-    return requests.count()
+        return emails
 
-def requests_per_user(requests):
-    """Get amount of requests submitted per user. 
-    
-    Args:
-        requests: all requests from database.
-    Returns:
-        count of requests per user
-    """
-    kv = {}
+    def unique_users(self):
+        """Get emails from requests. 
+        
+        Args:
+            requests: all requests from database.
+        Returns:
+            list of unique users.
+        """
+        uniq = set()
+        for request in self.requests.find():
+            email = str(request['email']).lower()
+            uniq.add(email)
+        
+        return uniq
 
-    for request in requests.find():
-        email = str(request['email']).lower() # doesn't consider unicode comparison
-        kv[email] = kv.get(email, 0) + 1
-    
-    return kv
+    def total_requests(self):
+        """Get total number of requests from the requests.
+        
+        Args:
+            requests: all requests from database.
+        Returns:
+            number of requests
+        """
+        return self.requests.count()
 
-def selections_per_user_request(requests):
-    """Get amount of selected datasets per user. 
-    
-    Args:
-        requests: all requests from database.
-    Returns:
-        total selection made by each user
-    """
-    sel = {}
-    for request in requests.find():
-        email = str(request['email']).lower()
-        raster_data = request['raster_data']
-        sel[email] = sel.get(email, 0) + len(raster_data)
-    
-    return sel
+    def requests_per_user(self):
+        """Get amount of requests submitted per user. 
+        
+        Args:
+            requests: all requests from database.
+        Returns:
+            count of requests per user
+        """
+        kv = {}
 
-def get_organizations(requests):
-    """Get organizations from the requests.
-    
-    Args:
-        requests: all requests from database.
-    Returns:
-        list of organizations
-    """
-    orgs = set()
-    blacklist = ['gmail.com', 'yahoo.com', 'hotmail.com']
+        for request in self.requests.find():
+            email = str(request['email']).lower() # doesn't consider unicode comparison
+            kv[email] = kv.get(email, 0) + 1
+        
+        return kv
 
-    for request in requests.find():
-        org = str(request['email']).split('@')[1]
-        if org.lower() not in blacklist:
-            orgs.add( org )
-    
-    return orgs
+    def selections_per_user_request(self):
+        """Get amount of selected datasets per user. 
+        
+        Args:
+            requests: all requests from database.
+        Returns:
+            total selection made by each user
+        """
+        sel = {}
+        for request in self.requests.find():
+            email = str(request['email']).lower()
+            raster_data = request['raster_data']
+            sel[email] = sel.get(email, 0) + len(raster_data)
+        
+        return sel
 
-def get_requests(requests):
-    """Get requests from database.
-    
-    Args:
-        requests: all requests from database.
-    Returns:
-        list of requests
-    """
-    return requests.find()
+    def get_organizations(self):
+        """Get organizations from the requests.
+        
+        Args:
+            requests: all requests from database.
+        Returns:
+            list of organizations
+        """
+        orgs = set()
+        blacklist = ['gmail.com', 'yahoo.com', 'hotmail.com']
 
-def get_boundary_count(requests):
-    """Get count of boundaries. 
-    
-    Args:
-        requests: all requests from database.
-    Returns:
-        boundaries are their frequency
-    """
-    boundaries = {}
-    for request in requests.find():
-        boundary = request['boundary']['title']
-        boundaries[boundary] = boundaries.get(boundary,0) + 1
+        for request in self.requests.find():
+            org = str(request['email']).split('@')[1]
+            if org.lower() not in blacklist:
+                orgs.add( org )
+        
+        return orgs
 
-    return boundaries
+    def get_requests(self):
+        """Get requests from database.
+        
+        Args:
+            requests: all requests from database.
+        Returns:
+            list of requests
+        """
+        return self.requests.find()
 
-def get_boundaries(requests):
-    """Get all boundaries from the requests.
-    
-    Args:
-        requests: all requests from database.
-    Returns:
-        list of boundaries
-    """
-    ret = []
-    for request in requests.find():
-        ret.append(request['boundary']['title'])
-    return ret
+    def get_boundary_count(self):
+        """Get count of boundaries. 
+        
+        Args:
+            requests: all requests from database.
+        Returns:
+            boundaries are their frequency
+        """
+        boundaries = {}
+        for request in self.requests.find():
+            boundary = request['boundary']['title']
+            boundaries[boundary] = boundaries.get(boundary,0) + 1
 
-def get_boundary_request_info(requests, boundary):
-    """Get request information for a boundary.
-    
-    Args:
-        requests: all requests from database.
-        boundary: country/region
-    Returns:
-        list containing (# of times the boundary has been requested, map of {info requested about boundary, # of times this info was requested}).
-    """
-    ret = []
-    count = 0
-    infokv = {}
-    for request in requests.find():
-        if request['boundary']['title'].split(' ')[0] == boundary:
-            count += 1
-            for field in request['raster_data']:
-                info = field['title']
-                infokv[ info ] = infokv.get( info, 0 ) + 1 # count number of times this info has been requested
-                
-                #return years with number of times requested in those years
-    
-    ret.append(count)
-    ret.append(infokv)
+        return boundaries
 
-    return ret
+    def get_boundaries(self):
+        """Get all boundaries from the requests.
+        
+        Args:
+            requests: all requests from database.
+        Returns:
+            list of boundaries
+        """
+        ret = []
+        for request in self.requests.find():
+            ret.append(request['boundary']['title'])
+        return ret
+
+    def get_boundary_request_info(self, boundary):
+        """Get request information for a boundary.
+        
+        Args:
+            requests: all requests from database.
+            boundary: country/region
+        Returns:
+            list containing (# of times the boundary has been requested, map of {info requested about boundary, # of times this info was requested}).
+        """
+        ret = []
+        count = 0
+        infokv = {}
+        for request in self.requests.find():
+            if request['boundary']['title'].split(' ')[0] == boundary:
+                count += 1
+                for field in request['raster_data']:
+                    info = field['title']
+                    infokv[ info ] = infokv.get( info, 0 ) + 1 # count number of times this info has been requested
+                    
+                    #return years with number of times requested in those years
+        
+        ret.append(count)
+        ret.append(infokv)
+
+        return ret
+
+    def get_request_count_for_dataset(self, name):
+        count = 0
+        for request in self.requests.find():
+            raster_data = request['raster_data']
+            for r in raster_data:
+                n = r['name']
+                if n == name:
+                    count += 1
+
+        return count
--- geoquery_requests.py
+++ geoquery_requests.py
@@ -6,7 +6,7 @@ import pymongo
 import json
 
 from pymongo import MongoClient
-from geoquery_queries import *  
+from geoquery_queries import GeoQueryRequests
 
 try:
     client = MongoClient('localhost', 27017)
@@ -18,7 +18,23 @@ db = client.asdf
 requests = db.det
 data = db.data
 
+rq = GeoQueryRequests(requests)
+
+children = []
+
 for d in data.find():
     name = d.get('name')
-    if name == "dist_to_groads":
-        print d
+    if name:
+        count = rq.get_request_count_for_dataset(name)
+        child = {"name": name, "requests": count}
+        children.append(child)
+
+print children
+
+# for r in requests.find():
+#     ra = r['raster_data']
+#     for d in ra:
+#         for da in data.find({'name': d.get('name')}):
+#             print d.get('name')
+        
+#         print 'Total: {0} VS {1}'.format(data.count(), data.find({'name': d.get('name')}).count())
